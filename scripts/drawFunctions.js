1805f31075a49f68f51e0d84f79aee77de634d19
rubenTito <rperez@cvc.uab.es>
2019-06-14 09:59:45 +0200

    HTML changed and CSS cleaned

--- scripts/drawFunctions.js
+++ scripts/drawFunctions.js
@@ -1,9 +1,9 @@
 //Changes the source attribute of the image and calls the function to print all Bounding Boxes.
 function plotImage(imgUrl) {
-	$("#qaImage").attr("src", imgUrl);
+	$("#qaImage").attr("src", "https://raw.githubusercontent.com/rubenTito/AMT_ST-VQA/master/images/" + imgUrl);
 
-	// Get image dimensions before loading it completely, compute aspect ratio and change image dimenions.
-	img = document.getElementById("qaImage");
+	// Get image dimensions before loading it completely, compute aspect ratio and change image dimensions.
+	let img = document.getElementById("qaImage");
 
 	img.onload = function() {
 		//Saves the original image size to get the difference when resizing.
--- scripts/formFunctions.js
+++ scripts/formFunctions.js
@@ -1,24 +1,3 @@
 function hideQA(idxQA){
 	$("#QADiv" + idxQA.toString()).hide();
 }
-
-function fillQAs(qa_list) {
-	for (let i = 0; i < qa_list.length; i++) {
-		qa = qa_list[i];
-		question = qa['question'];
-		answers = qa['answer'];
-		answer = answers[0];
-
-		$("#question" + i.toString()).val(question);
-		$("#answerOri" + i.toString()).val(answer);
-
-		if (answers.length == 2) {
-			verification = answers[1];
-			$("#answerVeri" + i.toString()).val(verification);
-		}
-	}
-
-	for (let i = qa_list.length; i <= maxQA; i++) {
-		hideQA(i);
-	}
-}
