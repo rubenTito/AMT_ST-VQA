188ff8b5d761cbddeaacf01919430d826ad9bac6
Rubèn Pérez Tito <ruben.perezt@e-campus.uab.cat>
2020-09-18 11:53:52 +0200

    Trying to fix instruction expand-collapse.

--- scripts/instructionsFunctions.js
+++ scripts/instructionsFunctions.js
@@ -3,28 +3,28 @@ $(document).ready(function() {
 
 
 	// Instructions expand/collapse
-	var content1 = $('#instructionBody1');
-	var trigger1 = $('#collapseTrigger1');
-	var content2 = $('#instructionBody2');
-	var trigger2 = $('#collapseTrigger2');
+	var content = $('#instructionBody');
+	var trigger = $('#collapseTrigger');
+	// var content2 = $('#instructionBody2');
+	// var trigger2 = $('#collapseTrigger2');
 
-	content1.hide();
-	content2.hide();
+	content.hide();
+	// content2.hide();
 
-	$('#collapse-text1').text('(Click to expand)');
-	$('#collapse-text2').text('(Click to expand)');
+	$('#collapse-text').text('(Click to expand)');
+	// $('#collapse-text2').text('(Click to expand)');
 
-	trigger1.click(function(){
-		content1.toggle();
-		var isVisible = content1.is(':visible');
+	trigger.click(function(){
+		content.toggle();
+		var isVisible = content.is(':visible');
 		if(isVisible){
-			$('#collapse-text1').text('(Click to collapse)');
+			$('#collapse-text').text('(Click to collapse)');
 		}else{
-			$('#collapse-text1').text('(Click to expand)');
+			$('#collapse-text').text('(Click to expand)');
 		}
 	});
 
-	trigger2.click(function(){
+	/*trigger2.click(function(){
 		content2.toggle();
 		var isVisible = content2.is(':visible');
 		if(isVisible){
@@ -32,6 +32,6 @@ $(document).ready(function() {
 		}else{
 			$('#collapse-text2').text('(Click to expand)');
 		}
-	});
+	});*/
 	// end expand/collapse
 });
