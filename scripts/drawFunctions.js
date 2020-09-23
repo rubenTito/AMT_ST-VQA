21516b7d834530a7883e015b48eefd6f05fb569d
Rubèn Pérez Tito <ruben.perezt@e-campus.uab.cat>
2020-09-23 17:09:05 +0200

    Link to images changed to 'Dev' because in 'production' there was a varying code.

--- scripts/drawFunctions.js
+++ scripts/drawFunctions.js
@@ -1,7 +1,8 @@
 //Changes the source attribute of the image and calls the function to print all Bounding Boxes.
 function plotImage(imgUrl) {
-	// $("#qaImage").attr("src", "https://raw.githubusercontent.com/rubenTito/AMT_ST-VQA/master/images/" + imgUrl);
-	$("#qaImage").attr("src", "https://rawcdn.githack.com/rubenTito/AMT_ST-VQA/b84d6f6878630383ee50b978c5845d98564274b8/images/" + imgUrl);
+	// $("#qaImage").attr("src", "https://raw.githubusercontent.com/rubenTito/AMT_ST-VQA/master/images/" + imgUrl);		// Previous github version
+	$("#qaImage").attr("src", "https://raw.githack.com/rubenTito/AMT_ST-VQA/master/images/" + imgUrl);
+	// $("#qaImage").attr("src", "https://rawcdn.githack.com/rubenTito/AMT_ST-VQA/b84d6f6878630383ee50b978c5845d98564274b8/images/" + imgUrl);	// Do not use this one since the random code varies!!
 
 	// Get image dimensions before loading it completely, compute aspect ratio and change image dimensions.
 	let img = document.getElementById("qaImage");
